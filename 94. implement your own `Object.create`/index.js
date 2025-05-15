f0297dfdddb3e6ec493d07560cb7adf7cc57855c
vigneshpamu <vigneshpamu2002@gmail.com>
2025-05-15 23:57:16 +0530

    Pushed

---
+++ 94. implement your own `Object.create`/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {any} proto
+ * @return {object}
+ */
+function myObjectCreate(proto) {
+  if (typeof proto !== 'object' || proto === null) {
+    throw new Error('Argument must be a non-null object')
+  }
+
+  function TempConstructor() {}
+  TempConstructor.prototype = proto
+  return new TempConstructor()
+}
