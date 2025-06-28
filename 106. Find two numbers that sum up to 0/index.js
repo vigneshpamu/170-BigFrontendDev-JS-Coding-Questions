dfb622d9acf7b2efcddb9e348a1251e6594f29aa
vigneshpamu <vigneshpamu2002@gmail.com>
2025-06-28 21:09:22 +0530

    sd

--- 106. Find two numbers that sum up to 0/index.js
+++ 106. Find two numbers that sum up to 0/index.js
@@ -1,19 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
 function findTwo(arr) {
   // your code here
-
   if (arr.length < 1) return null
   if (arr.length === 1) return null
+  let length = arr.length
   const obj = {}
 
-  for (let i = 0; i < arr.length; i++) {
-    const currentNum = arr[i]
-    const complement = 0 - currentNum
+  for (let i = 0; i < length; i++) {
+    const num = arr[i]
+    const complement = 0 - num
 
     if (obj[complement] !== undefined) {
       return [i, obj[complement]]
     }
 
-    obj[currentNum] = i
+    obj[num] = i
   }
 
   return null
